9851ce9d09485b524797df08eeec7c4acb5fd39c
almalee24 <flores.almalee@gmail.com>
2020-11-05 14:18:29 +0200

    add search bar

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,4 +2,13 @@ class PositionsController < ApplicationController
     def index
        @positions = Position.all
     end
+
+    def search
+        
+    end
+
+    private 
+    def position_params
+        params.require(:postion).permit(:title, :descritption, :company, :location, :url)
+    end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,2 +1,16 @@
 class Position < ApplicationRecord
+    belongs_to :user
+
+    def self.search(search)
+        if search 
+            keyword = Position.find_by(title: search)
+            if keyword
+                self.where(user_id: keyword)
+            else   
+                Position.all
+            end
+        else  
+            Position.all
+        end
+    end
 end
